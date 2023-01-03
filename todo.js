6a3efebbfff817ef49c92b72747bfc71f8889299
Esakkiraja1837 <esakkiraja.esakkipandi@ideas2it.com>
2023-01-03 10:47:14 +0530

    list-added

--- todo.js
+++ todo.js
@@ -1,3 +1,28 @@
-let date = new Date();
-let currentDate = date.toDateString();
-document.getElementById("date").innerHTML=currentDate;
+(function(){
+    function init(){
+        getDate();
+        addList();
+    }
+
+
+    function getDate(){
+        let date = new Date();
+        let currentDate = date.toDateString();
+        document.getElementById("date").innerHTML=currentDate;
+    }
+
+    function addList(){
+        var task = document.getElementById("Input");
+        task.addEventListener("keypress", function(event){
+            if(event.key === "Enter") {
+                var node = document.createElement("li");
+                var text = document.createTextNode(task.value);
+                node.innerHTML="list";
+                node.appendChild(text);
+                document.getElementById("mylists").appendChild(node);
+                task.value="";
+            }
+        });
+    }
+init();
+})();
